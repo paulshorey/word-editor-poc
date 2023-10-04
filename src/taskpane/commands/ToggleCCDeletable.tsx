e8d85a83443cef00024adaf5ef6cb9228e4e9dc2
Karsten <karsten.andersen@comcast.com>
2023-10-04 14:30:46 -0700

    Adding code that toggle READONLY mode for the CONTENT CONTROLS with color code. GREEN allow Editing/Deleting, RED dis-allow Editing/Deleting

---
+++ src/taskpane/commands/ToggleCCDeletable.tsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+import { DefaultButton } from "@fluentui/react";
+
+import { contextLoad } from "./commandUtils";
+/* global Word, require */
+
+const handleClick = (tagName: string, isDeletable: boolean) => {
+  return Word.run(async (context) => {
+    const controls = context.document.contentControls.getByTag(tagName);
+    contextLoad(context, controls);
+    context
+      .sync()
+      .then(() => {
+        controls.items.forEach((control) => {
+          context.load(control);
+          context.sync();
+          control.color = isDeletable ? "red" : "green";
+          control.cannotDelete = isDeletable;
+          control.cannotEdit = isDeletable;
+        });
+      })
+      .catch((e) => {
+        // eslint-disable-next-line no-undef
+        console.log("===>", e);
+      });
+
+    return context.sync();
+  });
+};
+
+interface ToggleCCDeletableInterface {
+  tagName: string;
+}
+const ToggleCCDeletable = ({ tagName }: ToggleCCDeletableInterface) => {
+  const [deletable, setDeletable] = useState<boolean>();
+  const [toggleStyle, setToggleStyle] = useState("faf-isDeletable");
+  useEffect(() => {
+    setToggleStyle(deletable ? "faf-isDeletable" : "faf-isNotDeletable");
+  }, [deletable]);
+
+  return (
+    <DefaultButton
+      className={`ms-welcome__action ${toggleStyle}`}
+      iconProps={{ iconName: "ChevronRight" }}
+      onClick={() => {
+        handleClick(tagName, deletable);
+        setDeletable((prev) => !prev);
+      }}
+    >
+      Toggle CC Deletable
+    </DefaultButton>
+  );
+};
+
+export default ToggleCCDeletable;
