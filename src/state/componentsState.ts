cf203e273a3f04d8b9a7513c3240ea56624848d3
Karsten <karsten.andersen@comcast.com>
2023-10-19 09:30:56 -0700

    This is a "kind-of" rollback to a version that has success in a lot of cases - not 100% though!

--- src/state/componentsState.ts
+++ src/state/componentsState.ts
@@ -1,20 +1,13 @@
 /* eslint-disable office-addins/no-context-sync-in-loop */
-/* global console, setTimeout, Office, document, Word, require */
+/* global setTimeout, Office, document, Word, require */
 import { create } from "zustand";
 import { TAGNAMES } from "@src/constants/constants";
 import { ComponentTestData } from "@src/testdata/TestData";
-import Poc2 from "@src/testdata/Poc2";
-import Don1 from "@src/testdata/Don1";
-import DummyXml from "@src/testdata/DummyXmlTag";
-import DummyXmlContent from "@src/testdata/DummyXmlContent";
-
-const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 /**
  * contentControl.id; context.document.contentControls.getById(id)
  */
 export type id = number;
-
 /**
  * contentControl.tag; context.document.contentControls.getByTag(tag);
  */
@@ -71,102 +64,78 @@ const componentsState = create((set, _get) => ({
   /**
    * Add a dataElement to the template, into the current cursor selection
    */
-  insertTag: function (documentName: string): void {
-    Word.run(async (context) => {
-      // 0. Get base64 data content
-      let isXML = false;
-      let contentToInsert;
-      switch (documentName) {
-        case "comp_with_table":
-          contentToInsert = ComponentTestData.comp_with_table.data;
-          break;
-        case "comp_simple_word":
-          contentToInsert = ComponentTestData.comp_simple_word.data;
-          break;
-        case "poc2":
-          contentToInsert = Poc2;
-          break;
-        case "don1":
-          contentToInsert = Don1;
-          break;
-        case "DummyXml":
-          isXML = true;
-          contentToInsert = DummyXml;
-          break;
-        case "DummyXmlContent":
-          isXML = true;
-          contentToInsert = DummyXmlContent;
-          break;
-        default:
-          Promise.reject("ERROR - Document does not exist");
-          return;
-      }
+  insertTag: function (documentName: string) {
+    return new Promise((resolve) => {
+      Word.run(async (context) => {
+        // 0. Get base64 data content
+        let base64DataContent;
+        switch (documentName) {
+          case "comp_with_table":
+            base64DataContent = ComponentTestData.comp_with_table.data;
+            break;
 
-      // 1. Insert into document
-      //
-      const contentRange = context.document.getSelection().getRange("Content");
-      const contentControl = contentRange.insertContentControl();
-      contentControl.tag = TAGNAMES.component; // `COMPONENT#${loadDocument}#${timeStamp}`
-      contentControl.title = documentName.toUpperCase();
-      contentControl.insertHtml("<div>Loading component content...</div>", "Start");
-      await context.sync();
-      const range = isXML
-        ? contentControl.insertOoxml(contentToInsert, "Replace")
-        : contentControl.insertFileFromBase64(contentToInsert, "Replace");
-      // contentControl.cannotEdit = true;
-      await range.context.sync();
-      await context.sync();
-      // insert line break if there is no text before
-      const rangeBefore = contentControl.getRange("Before");
-      const textBefore = rangeBefore.getTextRanges([" "], true).load();
-      textBefore.load("items");
-      await context.sync();
-      if (textBefore.items.length === 0) {
-        contentControl.insertBreak("Line", "Before");
-        await context.sync();
-      }
-      // insert line break if there is no text after
-      const rangeAfter = contentControl.getRange("After");
-      const textAfter = rangeAfter.getTextRanges([" "], true).load();
-      textAfter.load("items");
-      await context.sync();
-      if (textAfter.items.length === 0) {
-        contentControl.insertBreak("Line", "After");
-        await context.sync();
-      }
-      // sync
-      await context.sync();
-      context.document.body.load();
-      context.document.load();
+          case "comp_simple_word":
+            base64DataContent = ComponentTestData.comp_simple_word.data;
+            break;
 
-      //
-      // const contentControl = contentRange.getRange("After").insertContentControl();
-      // contentControl.tag = TAGNAMES.component; // `COMPONENT#${loadDocument}#${timeStamp}`
-      // contentControl.title = documentName.toUpperCase();
-      // contentControl.color = "#666666";
-      // contentControl.cannotDelete = false;
-      // contentControl.cannotEdit = false;
-      // contentControl.appearance = "BoundingBox";
-      // contentControl.clear();
-      // await context.sync();
-      // contentControl.insertHtml("<h1>contentControl</h1>", "Start");
-      // await context.sync();
-      // contentControl.insertFileFromBase64(base64DataContent, "Start");
-      // await context.sync();
-      // contentControl.insertText(" ", "Replace");
-      // await context.sync();
-      // contentControl.clear();
-      // await context.sync();
-      // contentControl.insertFileFromBase64(base64DataContent, "Replace");
-      // await context.sync();
-      // contentControl.insertHtml("<hr />", "End");
-      // await context.sync();
-      // 2. Update state
-      this.loadAll();
-      return context.sync();
-    }).catch(async (error) => {
-      console.log("Error: " + error);
-      console.log("Debug info: " + JSON.stringify(error?.debugInfo || ""));
+          default:
+            Promise.reject("ERROR - Document does not exist");
+            return;
+        }
+        // 1. Insert into document
+        const contentRange = context.document.getSelection().getRange();
+        const contentControl = contentRange.insertContentControl();
+        contentControl.set({
+          tag: TAGNAMES.component, // `COMPONENT#${loadDocument}#${timeStamp}`
+          title: documentName.toUpperCase(),
+        });
+        await context.sync();
+        contentControl.load("insertFileFromBase64");
+        await context.sync();
+        contentControl.insertFileFromBase64(base64DataContent, "Replace");
+        context
+          .sync()
+          .then((res) => {
+            // eslint-disable-next-line no-undef
+            console.log("===> RES:", res);
+          })
+          .catch((error) => {
+            // eslint-disable-next-line no-undef
+            console.log("===> Error", error);
+          });
+
+        const TIMEOUT = 2000;
+        setTimeout(() => {
+          // eslint-disable-next-line no-undef
+          console.log("===> RESET page", TIMEOUT);
+          const body = context.document.body;
+          body.load();
+
+          context
+            .sync()
+            .then(() => {
+              // eslint-disable-next-line no-undef
+              console.log("===> RELOAD", TIMEOUT);
+            })
+            .catch((error) => {
+              // eslint-disable-next-line no-undef
+              console.log("===> Error Clear", error);
+            });
+        }, TIMEOUT);
+        // 2. Update state
+        // const dataElement: dataElementType = {
+        //   id: contentControl.id,
+        //   tag: contentControl.tag,
+        //   title: documentName.toUpperCase(),
+        // };
+        // const state = get() as componentsStateType;
+        // set({
+        //   items: [dataElement, ...state.items],
+        // });
+        // await context.sync();
+        await this.loadAll();
+        resolve(true);
+      });
     });
   },
 
